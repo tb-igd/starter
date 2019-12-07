bdadd42f3b5239c2a435f30ade872ca87bfe49a7
renzyndrome <gabrielrebadulla@gmail.com>
2019-12-07 13:57:12 +0800

    functional toggle on/off audio

--- bubble/views.py
+++ bubble/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
 
 # Create your views here.
@@ -7,4 +6,5 @@ from django.views import View
 class Bubble(View):
 
     def get(self, request):
-        return HttpResponse('ok', status=200)
+        return render(request, 'bubble/noises.html')
+    
